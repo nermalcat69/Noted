b8abb35750f139d472caafb4be801d9deb0f7f0b
Arjun Aditya <73933669+nermalcat69@users.noreply.github.com>
2023-11-19 08:29:18 +0000

    zaduma starter

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,57 +1,27 @@
 import rss from "@astrojs/rss";
-import { blog } from "../lib/markdoc/frontmatter.schema";
-import { readAll } from "../lib/markdoc/read";
-import { SITE_TITLE, SITE_DESCRIPTION, SITE_URL } from "../config";
 
-export const get = async () => {
-  const posts = await readAll({
-    directory: "blog",
-    frontmatterSchema: blog,
+import type { PostProps } from "../types";
+
+const postImportResult = import.meta.glob<PostProps>("../../posts/**/*.mdx", {
+  eager: true,
+});
+const posts = Object.values(postImportResult);
+
+export const get = () =>
+  rss({
+    // `<title>` field in output xml
+    title: "",
+    // `<description>` field in output xml
+    description: "",
+    // base URL for RSS <item> links
+    // SITE will use "site" from your project's astro.config.
+    site: "https://haspar.us",
+    // list of `<item>`s in output xml
+    // simple example: generate items for every md file in /src/pages
+    // see "Generating items" section for required frontmatter and advanced use cases
+    items: posts.map(({ frontmatter }) => ({
+      title: frontmatter.title,
+      link: frontmatter.path,
+      pubDate: new Date(frontmatter.date),
+    })),
   });
-
-  const sortedPosts = posts
-    .filter((p) => p.frontmatter.draft !== true)
-    .sort(
-      (a, b) =>
-        new Date(b.frontmatter.date).valueOf() -
-        new Date(a.frontmatter.date).valueOf()
-    );
-
-  let baseUrl = SITE_URL;
-  // removing trailing slash if found
-  // https://example.com/ => https://example.com
-  baseUrl = baseUrl.replace(/\/+$/g, "");
-
-  const rssItems = sortedPosts.map(({ frontmatter, slug }) => {
-    if (frontmatter.external) {
-      const title = frontmatter.title;
-      const pubDate = frontmatter.date;
-      const link = frontmatter.url;
-
-      return {
-        title,
-        pubDate,
-        link,
-      };
-    }
-
-    const title = frontmatter.title;
-    const pubDate = frontmatter.date;
-    const description = frontmatter.description;
-    const link = `${baseUrl}/blog/${slug}`;
-
-    return {
-      title,
-      pubDate,
-      description,
-      link,
-    };
-  });
-
-  return rss({
-    title: SITE_TITLE,
-    description: SITE_DESCRIPTION,
-    site: baseUrl,
-    items: rssItems,
-  });
-};
